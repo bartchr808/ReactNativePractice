3e4432852301beb46e5193209d33e6dc03d19d3f
Bart Chrzaszcz <bart.chr@gmail.com>
2016-12-26 19:31:39 -0500

    Update SearchPage.js file

---
+++ PropertyFinder/SearchPage.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableHighlight,
+  ActivityIndicator,
+  Image
+} from 'react-native';
