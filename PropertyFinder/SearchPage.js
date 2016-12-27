35182f0b043ce9ca3a36908db32301f3dcf07374
Bart Chrzaszcz <bart.chr@gmail.com>
2016-12-27 02:27:20 -0500

    Added search bar and complimentary buttons.

--- PropertyFinder/SearchPage.js
+++ PropertyFinder/SearchPage.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Imports
 import React, { Component } from 'react'
 import {
   StyleSheet,
@@ -10,3 +11,103 @@ import {
   ActivityIndicator,
   Image
 } from 'react-native';
+
+// StyleSheet
+var styles = StyleSheet.create({
+  description: {
+    marginBottom: 20,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565'
+  },
+  container: {
+    padding: 30,
+    marginTop: 65,
+
+    alignItems: 'center'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center',
+  },
+  button: {
+    height: 36,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  buttonGo: {
+    flex: 1
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 3,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC'
+  }
+});
+
+// <View>
+//   <Text>hello</Text>
+// </View>
+
+
+class SearchPage extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          Search for houses to buy!
+        </Text>
+        <Text style={styles.description}>
+          Search by place-name, postcode or search near your location.
+        </Text>
+        <View style={styles.flowRight}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder='Search via name or postcode'/>
+          <TouchableHighlight style={StyleSheet.flatten([styles.button, styles.buttonGo])}
+            underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Go</Text>
+          </TouchableHighlight>
+        </View>
+
+          <TouchableHighlight style={styles.button}
+            underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Locatvvvvion</Text>
+          </TouchableHighlight>
+
+      </View>
+    );
+
+    // return (
+    //   <View style={{flex:1}}>
+    //     <View style={{flex:2, backgroundColor:'red'}}/>
+    //     <View style={{flex:1,flexDirection:'row'}}>
+    //       <View style={{flex:2, backgroundColor:'green'}}/>
+    //       <View style={{flex:1, backgroundColor:'purple'}}/>
+    //     </View>
+    //   </View>
+    //);
+  }
+}
+
+
+// This exports the SearchPage class, which permits its use in other files.
+module.exports = SearchPage;
