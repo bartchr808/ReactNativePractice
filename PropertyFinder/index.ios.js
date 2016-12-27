d7782c65bae0f9b2a7e06a51220c1de8c0539755
Bart Chrzaszcz <bart.chr@gmail.com>
2016-12-26 19:25:25 -0500

    Added navigation to initial route/main page and added SearchPage.js file

--- PropertyFinder/index.ios.js
+++ PropertyFinder/index.ios.js
@@ -4,20 +4,38 @@ var React = require('react')
 var ReactNative = require('react-native')
 
 var styles = ReactNative.StyleSheet.create({
-  text: {
-    color: 'black',
-    backgroundColor: 'white',
-    fontSize: 30,
-    margin: 80
-  }
+    text: {
+        color: 'black',
+        backgroundColor: 'white',
+        fontSize: 30,
+        margin: 80
+    },
+    container: {
+        flex: 1
+    }
 });
 
-class PropertyFinderApp extends React.Component {
-  render() {
-    return React.createElement(ReactNative.Text, {style: styles.text}, "Hello World!");
-  }
+class HelloWorld extends React.Component {
+    render() {
+        return <ReactNative.Text style={styles.text}>Hello World (Again)</ReactNative.Text>;
+    }
 }
 
+/*
+This constructs a navigation controller, applies a style and sets the initial route to the HelloWorld component.
+ */
+class PropertyFinderApp extends React.Component {
+    render() {
+        return (<ReactNative.NavigatorIOS style={styles.container} initialRoute={{
+            title: 'Property Finder',
+            component: HelloWorld
+        }}/>);
+    }
+}
 
-
-ReactNative.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
+/*
+Don't remove: AppRegistry defines the entry point to the application and provides the root component.
+ */
+ReactNative.AppRegistry.registerComponent('PropertyFinder', function() {
+    return PropertyFinderApp
+});
